668d551a90cc0bcb12f4c01e254d4ac070a644a9
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2021-12-04 17:08:32 +0100

    PoC for searchbar
    This is a PoC and will be refactored once a service for getting coin data has been made

--- client/src/app/components/search/search.component.ts
+++ client/src/app/components/search/search.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, OnInit, Output, ViewChild, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 import { Coin } from 'src/app/shared/models/coin.model';
 
 @Component({
@@ -9,12 +10,26 @@ import { Coin } from 'src/app/shared/models/coin.model';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  search = new FormControl();
+  coins: Coin[] = [{ name: 'Bitcoin', symbol: 'BTC' }, { name: 'Ethereum', symbol: 'ETH' }, { name: 'Litecoin', symbol: 'LTC' }];
+  filteredCoins!: Observable<Coin[]>;
 
-
-  searchText: string = '';
   constructor() { }
 
   ngOnInit(): void {
+    this.filteredCoins = this.search.valueChanges.pipe(
+      startWith(''),
+      map(value => (typeof value === 'string' ? value : value.name)),
+      map(name => (name ? this._filter(name) : this.coins.slice())),
+    )
   }
 
+  displayFn(coin?: Coin): string {
+    return coin && coin.name ? coin.name : '';
+  }
+
+  private _filter(coin: string): Coin[] {
+    const filterValue = coin.toUpperCase();
+    return this.coins.filter(coin => coin.name.toUpperCase().indexOf(filterValue));
+  }
 }
