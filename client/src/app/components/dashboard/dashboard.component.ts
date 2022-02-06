d4fcc16b35ea1c3ea1807035c47c0a9246d0138c
Thomas Maneschijn <thomasmaneschijn@gmail.com>
2022-02-06 22:46:27 +0100

    Add Angolia instant search

--- client/src/app/components/dashboard/dashboard.component.ts
+++ client/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth.service';
+import algoliasearch from 'algoliasearch/lite';
+
+const searchClient = algoliasearch(
+  '730ORYS7RD',
+  '241da836d6bc2129ca7bad6d2ee2922f'
+);
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +13,11 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  config = {
+    indexName: 'coins',
+    searchClient
+  };
+
   constructor(public authService: AuthService) {}
 
   ngOnInit(): void {}
